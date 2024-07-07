7852de3be6cd4a448e157da3c36ccaedbba46674
alnandoh <alnandoharsono@gmail.com>
2024-07-07 21:53:07 +0700

    feat: implement react query client provider

---
+++ libs/utils/Provider.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactNode } from "react";
+
+interface QueryClientProviderProps {
+  children: JSX.Element | ReactNode;
+}
+
+export default function Provider({ children }: QueryClientProviderProps) {
+  const queryClient = new QueryClient();
+
+  return (
+    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+  );
+}
