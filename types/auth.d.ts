3186d694752b361f14a466e4b7be69abe5ca2600
alnandoh <alnandoharsono@gmail.com>
2024-07-18 12:38:25 +0700

    fix: update on auth typing to receive token

--- types/auth.d.ts
+++ types/auth.d.ts
@@ -3,10 +3,12 @@ import NextAuth, { DefaultSession } from "next-auth";
 declare module "next-auth" {
   interface User {
     role?: string;
+    token?: string;
   }
   interface Session {
     user: {
       role?: string;
+      token?: string;
     } & DefaultSession["user"];
   }
 }
